d535d42ed3d16691fc63f8456e2550f62c6aa1b0
Trung Hieu <hieu@ltv.dev>
2023-03-25 12:10:37 +0700

    Create and Styling TaskDetails component

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,24 @@
       <img src="./assets/pinia-logo.svg" alt="pinia-logo">
       <h1>{{ taskStore.name }} Task</h1>
     </header>
+
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
 import {useTaskStore} from './stores/TaskStore';
+import TaskDetails from './components/TaskDetails.vue';
 
   export default {
+    components: {
+      TaskDetails
+    },
     setup() {
       const taskStore = useTaskStore();
 
