6ab14efacfe656e7b0c1e3b3190760562d3756e2
Trung Hieu <hieu@ltv.dev>
2023-03-25 11:57:55 +0700

    Accessing the store state

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,22 @@
     <!-- heading -->
     <header>
       <img src="./assets/pinia-logo.svg" alt="pinia-logo">
-      <h1>Pinia Task</h1>
+      <h1>{{ taskStore.name }} Task</h1>
     </header>
   </main>
 </template>
 
 <script>
+import {useTaskStore} from './stores/TaskStore';
+
   export default {
-    
+    setup() {
+      const taskStore = useTaskStore();
+
+      return {
+        taskStore
+      }
+    }
   }
 </script>
 
